3a7b4d77d637f60d157fd49a7a6bd082d9120b99
MariDV <m.vilela33@gmail.com>
2019-09-30 10:37:15 -0700

    first commit

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    public static void main(String[]args){
+        System.out.println("Hello Jose");
+        System.out.print("Hello");
+        System.out.print("Jose");
+
+        String any="Momo nhoco-nhoco";
+        System.out.println("Hello " + any);
+        
+    }
+}
