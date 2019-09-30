10384a63753540564606adcddd6c542c97389bf7
MariDV <m.vilela33@gmail.com>
2019-09-30 11:21:00 -0700

    int short byte

--- src/Hello.java
+++ src/Hello.java
@@ -3,9 +3,14 @@ public class Hello {
         System.out.println("Hello Jose");
         System.out.print("Hello");
         System.out.print("Jose");
-
         String any="Momo nhoco-nhoco";
         System.out.println("Hello " + any);
-        
+
+        byte num=Byte.MAX_VALUE;
+        short shortNumber= Short.MAX_VALUE;
+
+        System.out.println("byte number value is:" +num);
+        System.out.println("short number value is:" + shortNumber);
     }
 }
+ 
